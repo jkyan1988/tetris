f9eb7f7750d5d9db7e6249cacb832b43d1b8e685
James Kyan <james.kyan88@gmail.com>
2022-09-22 11:42:46 -0400

    added event listeners

--- app.js
+++ app.js
@@ -44,15 +44,105 @@ document.addEventListener("DOMContentLoaded", () => {
     const theTetriminoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
     let currentPosition = 4
-    let current = theTetriminoes[0][0]
+    let currentRotation = 0
 
-    //draw the first rotations in the first teromino
+    // randomly select a Tetromino and its first rotation
+    let random = Math.floor(Math.random() *theTetriminoes.length)
+    let current = theTetriminoes[random][0]
+
+    //draw the teromino
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino')
         })
     }
 
-    draw()
+    //undraw the Tetromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    // make the tetromino move down every second
+    timerId = setInterval(moveDown, 1000)
+
+    //assign functions to keyCodes
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        } else if(e.keyCode === 38) {
+            rotate()
+        } else if(e.keyCode === 39) {
+            moveRight()
+        }
+        else if(e.keyCode === 40) {
+            moveDown()
+        }
+    }
+    document.addEventListener('keyup', control)
+
+    //move down function
+    function moveDown() {
+        undraw()
+        currentPosition += width
+        draw()
+        freeze()
+    }
+
+    //freeze function 
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+            //start a new tetromino falling
+            random = Math.floor(Math.random() * theTetriminoes.length)
+            current = theTetriminoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
+
+    //move the tetromino to the left, unless is at the edge or there is a blockage
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -= 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition += 1
+        }
+
+        draw()
+    }
+
+    // move the tetromino right, unless is at the edge or there is a blockage
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if(!isAtRightEdge) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -= 1
+        }
+
+        draw()
+    }
+
+    // rotate the tetromino
+    function rotate(){
+        undraw()
+        currentRotation ++
+        if(currentRotation === current.length){ //if the current rotation gets to 4, make it go back to 0
+            currentRotation = 0
+        }
+        current = theTetriminoes[random][currentRotation]
+        draw()
+    }
+
+
+
+
 
 })
